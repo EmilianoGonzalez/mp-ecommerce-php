3bfc57bf47a01da6f73abf9a3b555835aa02bb84
Emiliano Gonzalez <emiliano74@gmail.com>
2020-06-10 21:54:19 -0300

    no message

--- detail.php
+++ detail.php
@@ -57,7 +57,7 @@
         $item->title = $_POST['title'];
         $item->quantity = $_POST['unit'];
         $item->unit_price = $_POST['price'];
-        $item->img = $_POST['img'];
+        $item->img = "https://emilianogonzalezmp-commerce-ph.herokuapp.com/".$_POST['img'];
         $item->currency_id = "ARS";
 
         $preference->items = array($item);
